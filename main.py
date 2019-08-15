e7a6a9a7b8e781c279f2924e2626ed1ab24e7417
Itai Sevitt <isevitt@gmail.com>
2019-08-16 00:00:58 +0300

    first

---
+++ main.py
@@ -0,0 +1,20 @@
+from lxml import html
+import requests
+import re
+import urllib.request
+from inscriptis import get_text
+
+def scrape():
+    url = "https://www.bbc.co.uk"
+    html = urllib.request.urlopen(url).read().decode('utf-8')
+    return html
+
+def get_list_words(html):
+    text = get_text(html)
+    chars_to_replace = ["*", "/", "\\", "?", "!",  "\t", "&", "'", "-", ".", "," "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "$", "©"]
+    for i in chars_to_replace:
+        text = text.replace(i, "")
+    text = text.replace("\n", " ")
+    text = re.sub(' +', ' ', text)
+    list_of_words = text.split(" ")
+    return list_of_words
